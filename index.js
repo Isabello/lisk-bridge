3c3a88823979446aeaa3d62ed3018c23924c7df5
isabello <isabella@lightcurve.io>
2017-07-27 17:18:41 +0200

    Adding Gruntfile and other test stuff

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const { Client } = require('pg')
-const client = new Client()
+
+const client = new Client({
+  user: '',
+  host: 'localhost',
+  database: 'lisk_main',
+  password: 'password',
+  port: 5432,
+})
 
 client.connect()
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  client.end()
-})
+// Defines count query
+var query = 'SELECT count(1) from blocks'
+
+// Executes database query with passed query
+function dbQuery(query) {
+  client.query(query, (err, res) => {
+    console.log(err ? err.stack : res.rows[0].count) // Returned count
+  })
+}
+
+// Gracefully close connections and exit
+process.on( 'SIGTERM', function () {
+   client.end(function () {
+     console.log("Closed DB connections");
+     console.log("Exiting lisk-bridge");
+   });
+});
+
+dbQuery(query);
