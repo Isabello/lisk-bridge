67277a97d6598f547e4df9278ea53aaa047ea1c1
isabello <isabella@lightcurve.io>
2017-07-28 16:02:21 +0200

    Add config helper

---
+++ helpers/config.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+// const path = require('path');
+const program = require('commander');
+
+function Config () {
+
+  program
+  	.version('0.1.0')
+  	.option('-s, --source <path>', 'Source install')
+  	.option('-t, --target <path>', 'Target install')
+  	.parse(process.argv);
+
+  if (program.source) {
+  	console.warn(fs.existsSync(program.source));
+  } else {
+  	console.warn('Source installation required');
+  	process.exit(2);
+  }
+
+  if (program.target) {
+  	console.warn(fs.existsSync(program.target));
+  } else {
+  	console.warn('Target installation required');
+  	process.exit(2);
+  }
+
+}
+
+module.export = Config;
