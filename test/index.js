b493d776f6727ca229fbe3031f4b4ef21fd6e9d6
isabello <isabella@lightcurve.io>
2017-07-28 10:56:46 +0200

    Add database name test

--- test/index.js
+++ test/index.js
@@ -3,19 +3,31 @@
 // Requires
 const node = require('./node.js');
 
+// Variables
+const queryBlocks = 'SELECT count(1) from blocks';
+const queryPgDatabase = 'select count(datname) from pg_database where datname = $1'
+
 describe('Connectivity Testing', () => {
 	// Describe function for DB query
-	function queryDatabase(query) {
-		node.dbQuery(query);
+	function queryDatabase(query, params) {
+		node.dbQuery(query, params);
 	}
 
 	describe('Check Postgres', () => {
-		it('using known query should be ok', (done) => {
-			const queryBlocks = 'SELECT count(1) from blocks';
-			queryDatabase(queryBlocks, (err, res) => {
-				node.expect(res.rows[0].count).to.be.a.number;
+
+		it('using pg_database table should be ok', (done) => {
+			queryDatabase(queryPgDatabase, node.client.database, (err, res) => {
+				node.expect(res.rows[0].count).to.equal(1);
 			});
 			done();
 		});
+
+    it('using count query against blocks should be ok', (done) => {
+      const queryBlocks = 'SELECT count(1) from blocks';
+      queryDatabase(queryBlocks, [], (err, res) => {
+        node.expect(res.rows[0].count).to.be.a.number;
+      });
+      done();
+    });
 	});
 });
